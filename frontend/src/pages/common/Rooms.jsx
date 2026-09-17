0d49aa203bd8df5826ec1bd6681e776869eb152c
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-09-17 22:39:05 +0530

    feat: fetch and display room list in Rooms component

--- frontend/src/pages/common/Rooms.jsx
+++ frontend/src/pages/common/Rooms.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import useRoomStore from '../../app/useRoomStore'
 
 const Rooms = () => {
+
+  const roomList = useRoomStore((state) => state.roomList)
+
+  console.log("roomList", roomList)
+
   return (
     <div>Rooms</div>
   )
